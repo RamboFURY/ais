5f27e8b2dfbd17c82738d2a0320fa40203230e6e
sahilnchiku <sahil.pathania.3979@gmail.com>
2020-03-20 04:56:06 +0530

    regform ready

--- dashform.php
+++ dashform.php
@@ -4,33 +4,42 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="icon" type="image/png" href="https://img.icons8.com/cute-clipart/128/000000/edit.png">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title></title>
+    <title>ED1T</title>
     <link rel="stylesheet" type="text/css" href="style.css">
-    <link rel="apple-touch-icon" sizes="180x180" href="images\apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="images\favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="images\favicon-16x16.png">
-    <link rel="manifest" href="images\site.webmanifest">
     <script src="https://kit.fontawesome.com/e394687089.js"></script>
 </head>
 <body>
-  <div class="navbar navbar-inverse navbar-fixed-top">
-              <div class="container">
-                    <div class="navbar-header">
-                      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                          <span class="icon-bar"></span>
-                          <span class="icon-bar"></span>
-                          <span class="icon-bar"></span>
-                      </button>
-                    <a href="dashboard.php" class="navbar-brand"></a>
-                    </div> 
-                    <div>
-                        <ul class="nav navbar-nav navbar-right">
-                        <li><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span></a></li>
-                    </div>
-              </div>
+  <div class="panel panel-primary">
+    <div class="panel-heading"><h3>Registration Form</h3></div>
+    <div class="panel-body">
+<form class="form-default" action="" method="post" name="register">
+  <div class="form-group">
+      <label for="name">Name:</label>
+      <input type="text" name="name" class="form-control" id="name" placeholder="please enter your complete name">
+      <label for="username">User Name:</label>
+      <input type="text" name="uname" class="form-control" id="uname" placeholder="enter the user name you have chosen">
+      <label for="email">E-Mail:</label>
+      <input type="text" name="email" class="form-control" id="email" placeholder="enter your E-MAIL">
+      <label for="dob">Date Of Birth:</label>
+      <input type="text" name="dob" class="form-control" id="dob" placeholder="enter your date of birth">
+      <label for="add">Address:</label>
+      <input type="text" name="add" class="form-control" id="add" placeholder="place of residence">
+      <label for="mob">Contact:</label>
+      <input type="text" name="mob" class="form-control" id="mob" placeholder="enter your mobile number">
+      <label for="pwd">Password:</label>
+      <input type="password" name="pwd" class="form-control" id="pwd" placeholder="create a strong password">
+      <label for="pwd">Renter-Password:</label>
+      <input type="password" name="pwd" class="form-control" id="pwd" placeholder="enter password again">
+      <div class="form-group">
+      <a href="#"><button type="submit" class="btn btn-primary btn-block" value="SUBMIT" name="submitbtn">Submit</button></a><br>
+      </div>
   </div>
-  <footer class="footer">
+</form>
+<p>Already Registered?<a href="#"><b> Click</b></a> here.</p>
+</div> </div> </div>
+<footer class="footer">
     <div class="container">
        <center>
           <p></p>
